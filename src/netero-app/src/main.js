d9983a6d1009d3062e7b261fe9dabddf6145d28d
gongahkia <angryapplegravy@gmail.com>
2025-02-26 16:17:36 +0800

    feat(app): added router

--- src/netero-app/src/main.js
+++ src/netero-app/src/main.js
@@ -1,4 +1,24 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import Register from './components/Register.vue'
+import CreateBallot from './components/CreateBallot.vue'
+import Vote from './components/Vote.vue'
+import Results from './components/Results.vue'
+
+const routes = [
+  { path: '/register', component: Register },
+  { path: '/create-ballot', component: CreateBallot },
+  { path: '/vote', component: Vote },
+  { path: '/results', component: Results }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
