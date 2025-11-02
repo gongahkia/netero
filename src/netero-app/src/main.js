66e1729e6058726c0cd59b3df636f4bd7762acfd
gongahkia <angryapplegravy@gmail.com>
2025-11-03 00:44:32 +0800

    feat(app): premium request added and kived

--- src/netero-app/src/main.js
+++ src/netero-app/src/main.js
@@ -1,13 +1,20 @@
 import { createApp } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
+import './styles/theme.css'
 
 import Register from './components/VoterRegistration.vue'
 import CreateBallot from './components/BallotCreation.vue'
 import Vote from './components/VotingInterface.vue'
 import Results from './components/ResultsDisplay.vue'
+import OrgDashboard from './components/OrgDashboard.vue'
+import MemberDashboard from './components/MemberDashboard.vue'
 
 const routes = [
+  { path: '/', redirect: '/member' },
+  { path: '/member', component: MemberDashboard },
+  { path: '/org', component: OrgDashboard },
+  // legacy/direct routes
   { path: '/register', component: Register },
   { path: '/create-ballot', component: CreateBallot },
   { path: '/vote', component: Vote },
